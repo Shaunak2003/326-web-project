1298c196361e9be9d2ce6ae23a7b74988de87db5
ak549 <60071579+ak549@users.noreply.github.com>
2024-04-02 20:14:00 -0400

    updating css and js

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    //h2 in main section
-    const mainH2 = document.querySelectorAll("main h2");
+document.addEventListener('DOMContentLoaded', function() {
+    //select all h2s
+    const sectionHeaders = document.querySelectorAll('main section h2');
 
-    // event listener for h2
-    mainH2.forEach(h2 => {
-        h2.addEventListener("click", function() {
-            // make paragraph visible
-            const paragraph = this.nextElementSibling;
-            paragraph.classList.toggle("expanded");
+    sectionHeaders.forEach(header => {
+        header.addEventListener('click', function() {
+            //get the content container
+            const container = this.parentElement.querySelector('.content-container');
+            if (container) {
+                container.classList.toggle('expanded');
+            }
         });
     });
 });
+
+
+
