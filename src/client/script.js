8b512313c9180261d389fb9cd21b299312389689
Shaunak2003 <shaunak.karmarkar@gmail.com>
2024-04-23 17:48:31 -0400

    Script.js confirm

--- src/client/script.js
+++ src/client/script.js
@@ -1,42 +1,49 @@
-var db = new PouchDB('books');
+var db = new PouchDB('items');
 var idCount = 0;
 
-// Function to save a book to the database
-function saveBook() {
+// Function to save a item to the database
+function saveItem() {
     // Get user input
     idCount++;
     console.log(idCount);
 
-    var bookName = document.getElementById('bookName').value;
-    var bookDescription = document.getElementById('bookDescription').value;
-    var bookPrice = parseFloat(document.getElementById('bookPrice').value);
-    var bookCondition = document.getElementById('bookCondition').value;
+    var itemName = document.getElementById('itemName').value;
+    var itemCategory = document.getElementById('itemCategory').value
+    var itemDescription = document.getElementById('itemDescription').value;
+    var itemPrice = parseFloat(document.getElementById('itemPrice').value);
+    var itemCondition = document.getElementById('itemCondition').value;
+    var itemImage = document.getElementById('itemImage').value
 
-    // Add the book object to the PouchDB database
+    // Add the item object to the PouchDB database
     db.put({
         "_id": `${idCount}`,
-        "name": bookName,
-        "description": bookDescription,
-        "price": bookPrice,
-        "condition": bookCondition
+        "category": itemCategory,
+        "name": itemName,
+        "description": itemDescription,
+        "price": itemPrice,
+        "condition": itemCondition,
+        "image": itemImage
     })
     .then(function (response) {
         //console.log('Item added successfully!');
         alert("Item added successfully");
-        document.getElementById('bookName').value = '';
-        document.getElementById('bookDescription').value = '';
-        document.getElementById('bookPrice').value = '';
-        document.getElementById('bookCondition').value = '';
+        document.getElementById('itemName').value = '';
+        document.getElementById('itemCategory').value = ''
+        document.getElementById('itemDescription').value = '';
+        document.getElementById('itemPrice').value = '';
+        document.getElementById('itemCondition').value = '';
+        document.getElementById('itemImage').value = ''
     })
     .catch(function (err) {
         console.log(err);
     });
 
-    // Retrieve and log the book
-    db.get(`${idCount}`).then(function (book) {
-        console.log(book);
+    // Retrieve and log the item
+    db.get(`${idCount}`).then( (item) => {
+        console.log(item);
     })
     .catch(function (err) {
         console.log(err);
     });
+    db.destroy()
 }
