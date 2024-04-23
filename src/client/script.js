97d69fcd20b1b446f370347946defc1acedff5b4
ak549 <60071579+ak549@users.noreply.github.com>
2024-04-23 17:27:13 -0400

    DB change for working ID

--- src/client/script.js
+++ src/client/script.js
@@ -1,10 +1,11 @@
 var db = new PouchDB('books');
-var idCount = 0
+var idCount = 0;
+
 // Function to save a book to the database
 function saveBook() {
     // Get user input
-    idCount++
-    console.log(idCount)
+    idCount++;
+    console.log(idCount);
 
     var bookName = document.getElementById('bookName').value;
     var bookDescription = document.getElementById('bookDescription').value;
@@ -13,33 +14,29 @@ function saveBook() {
 
     // Add the book object to the PouchDB database
     db.put({
-      "_id": "${idCount}",
-      "name": bookName,
-      "description": bookDescription,
-      "price": bookPrice,
-      "condition": bookCondition
+        "_id": `${idCount}`,
+        "name": bookName,
+        "description": bookDescription,
+        "price": bookPrice,
+        "condition": bookCondition
     })
-        .then(function (response) {
-            //console.log('Item added successfully!');
-            alert("Item added successfully")
-            document.getElementById('bookName').value = '';
-            document.getElementById('bookDescription').value = '';
-            document.getElementById('bookPrice').value = '';
-            document.getElementById('bookCondition').value = '';
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+    .then(function (response) {
+        //console.log('Item added successfully!');
+        alert("Item added successfully");
+        document.getElementById('bookName').value = '';
+        document.getElementById('bookDescription').value = '';
+        document.getElementById('bookPrice').value = '';
+        document.getElementById('bookCondition').value = '';
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 
-      db.get("'" + "${count}" + "'").then( (book) => console.log(book))
-      //db.destroy()
+    // Retrieve and log the book
+    db.get(`${idCount}`).then(function (book) {
+        console.log(book);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
 }
-
-/* db.allDocs({include_docs: true})
-  .then(function (result) {
-    // Log the documents to the console
-    console.log(result.rows.map(row => row.doc));
-  })
-  .catch(function (err) {
-    console.log(err);
-  }); */
