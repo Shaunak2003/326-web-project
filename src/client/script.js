bed68131f0e3519b05841e96070d4fe412448aeb
Shaunak2003 <shaunak.karmarkar@gmail.com>
2024-04-24 21:50:02 -0400

    search bar implementation

--- src/client/script.js
+++ src/client/script.js
@@ -1,25 +1,14 @@
 var db = new PouchDB('items');
-var idCount = 0
-console.log("id count before reload", idCount)
-// run a function that initializes/syncs with localsotrsge
 
-/* function syncWithLocalStorage() {
-  // Replicate data from PouchDB to local storage
-  return db.replicate.to(new PouchDB('itemsLocalStorage'))
-      .on('complete', function () {
-          console.log('Replication completed successfully');
-      })
-      .on('error', function (err) {
-          console.log('Error replicating data:', err);
-      });
-} */
+const bookButton = document.getElementById('book-button')
+const electronicsButton = document.getElementById('electronics-button')
+const fashionButton = document.getElementById('fashion-button')
+const searchButton = document.getElementById('button-input')
 
 // Function to save an item to the database
   function saveItem() {
       // Get user input
-      idCount++;
-      console.log("id count after save item", idCount);
-
+      var idCount = Math.floor(Math.random() * 10000000000);
       var itemName = document.getElementById('itemName').value;
       var itemCategory = document.getElementById('itemCategory').value
       var itemDescription = document.getElementById('itemDescription').value;
@@ -57,12 +46,12 @@ console.log("id count before reload", idCount)
       });
 
       // Retrieve and log the item
-      db.get(`${idCount}`).then( (item) => {
+      /* db.get(`${idCount}`).then( (item) => {
           console.log(item);
       })
       .catch(function (err) {
           console.log(err);
-      });
+      }); */
 
       
       //db.destroy()
@@ -80,69 +69,14 @@ function previewImage(event) {
     reader.readAsDataURL(input.files[0]);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    //var db = new PouchDB('items');
-
-    // Function to display items with the category "books"
-    function displayItems(category) {
-        db.allDocs({
-            include_docs: true,
-            attachments: true
-        }).then(function (result) {
-            var items = result.rows.filter(function (row) {
-              lowerCaseCategory = category.toLowerCase() //this is just for the products.html to display the 
-              //category of the product with the first letter capitalized
-                return row.doc.category === `${lowerCaseCategory}`;
-            });
-
-            var displayCategory = document.getElementById('display-category')
-            displayCategory.innerHTML = category
-
-            var productsContainer = document.getElementById('products-container');
-            productsContainer.innerHTML = '';
-
-            items.forEach(function (row) {
-                var product = row.doc;
-                var productHTML = `
-                    <div class="product">
-                        <img src="${product.image}" alt="${product.name}">
-                        <h3>${product.name}</h3>
-                        <p>Category: ${product.category}</p>
-                        <p>Description: ${product.description}</p>
-                        <p>Condition: ${product.condition}</p>
-                        <p class="price">Price: $${product.price}</p>
-                    </div>
-                `;
-                productsContainer.insertAdjacentHTML('beforeend', productHTML);
-            });
-        }).catch(function (err) {
-            console.log(err);
-        });
-    }
-
-    // Call the displayBooks function to populate the products page with books
-    var params = new URLSearchParams(window.location.search);
-    var category = params.get('category');
-
-    // Call the displayItems function with the category parameter
-    if (category) {
-        displayItems(category);
-    }
-});
-
-var temp = document.getElementById('search-input').value
-console.log(temp)
-
-function searchItems(){
-    var searchParam = document.getElementById('search-input').value
-    console.log(searchParam)
+function searchItems(searchParam){
+  console.log(searchParam)
     db.allDocs({
       include_docs: true,
       attachments: true
   }).then( (result) => {
     var items = result.rows.filter(function (row) {
-      lowerCaseSearchParam = searchParam.toLowerCase() 
-        return row.doc.name === `${lowerCaseSearchParam}`;
+        return row.doc.name.toLowerCase().includes(searchParam.toLowerCase())
     });
     var displayCategory = document.getElementById('display-category')
     displayCategory.innerHTML = 'Items with similar names'
@@ -169,3 +103,65 @@ function searchItems(){
             console.log(err);
         });
   }
+
+  function displayItems(category) {
+    db.allDocs({
+        include_docs: true,
+        attachments: true
+    }).then(function (result) {
+        var items = result.rows.filter(function (row) {
+          lowerCaseCategory = category.toLowerCase() //this is just for the products.html to display the 
+          //category of the product with the first letter capitalized
+            return row.doc.category === `${lowerCaseCategory}`;
+        });
+
+        var displayCategory = document.getElementById('display-category')
+        displayCategory.innerHTML = 'All ' + category
+
+        var productsContainer = document.getElementById('products-container');
+        productsContainer.innerHTML = '';
+
+        items.forEach(function (row) {
+            var product = row.doc;
+            var productHTML = `
+                <div class="product">
+                    <img src="${product.image}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                    <p>Category: ${product.category}</p>
+                    <p>Description: ${product.description}</p>
+                    <p>Condition: ${product.condition}</p>
+                    <p class="price">Price: $${product.price}</p>
+                </div>
+            `;
+            productsContainer.insertAdjacentHTML('beforeend', productHTML);
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+bookButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  document.getElementById('search-input').value = ''
+  displayItems('Books')
+})
+electronicsButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  document.getElementById('search-input').value = ''
+  displayItems('Electronics')
+})
+fashionButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  document.getElementById('search-input').value = ''
+  displayItems('Fashion')
+})
+searchButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  const searchParamInput = document.getElementById('search-input').value
+  if (searchParamInput.trim() !== ''){
+    window.location.href = 'products.html?name=' + encodeURIComponent(searchParamInput)
+  }
+  var search = new URLSearchParams(window.location.search)
+  var searchParam = search.get('name')
+  searchItems(searchParam)
+})
